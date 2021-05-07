35f0da71de6afeb416721551601de5809ba3c9be
YoshinoriKamiya <78638073+YoshinoriKamiya@users.noreply.github.com>
2021-05-07 22:44:19 +0900

    login (#3)

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+ 
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:success] = "Saved successfully!"
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = "Invalid content. Try again"
+      render 'new'
+      
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root 'static_pages#home'
   get '/signup',to:'users#new'
+
+  get '/login', to: 'sessions#new'
+  delete '/logout', to: 'sessions#destroy'
   
 end
