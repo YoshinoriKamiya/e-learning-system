b4707fa354cc258d08be2b80ce4407a066d58723
YoshinoriKamiya <78638073+YoshinoriKamiya@users.noreply.github.com>
2021-05-14 22:53:54 +0900

    dashboard (#6)
    dashbord
    
    follow_button
    
    follow_function
    
    made changes based from comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   before_action :only_loggedin_users, only: [:index,:edit,:update]
   before_action :correct_user, only: [:edit, :update]
- 
+  before_action :only_loggedin_users, only: [:index, :edit, :update, 
+                                              :destroy, :following, :followers]
 
   def index
     #@users = User.all
@@ -27,39 +28,53 @@ class UsersController < ApplicationController
     end
   end
   
-def show
-  @user = User.find(params[:id])
-end
+  def show
+    @user = User.find(params[:id])
+  end
 
-def update
-  @user = User.find(params[:id])
-  if @user.update_attributes(user_params)
-    flash[:success] = "Saved succesfully!"
-    redirect_to @user
-  else
-    flash[:danger] = "Invalid content. Try again"
-    render 'edit'
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Saved succesfully!"
+      redirect_to @user
+    else
+      flash[:danger] = "Invalid content. Try again"
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_url
+  end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page], per_page: 5)
+    @all_users = @user.followed_users
+    render 'show_follow'
+  end 
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page], per_page: 5)
+    @all_users = @user.followers
+    render 'show_follow'
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+  def only_loggedin_users
+    redirect_to login_url unless logged_in?
+ end
+   
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(roo_url) unless current_user?(@user)
   end
 end
-end
-
-def destroy
-  User.find(params[:id]).destroy
-  redirect_to users_url
-end
-
-
-private
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
-  
-    def only_loggedin_users
-       redirect_to login_url unless logged_in?
-     end
-   
-     def correct_user
-       @user = User.find(params[:id])
-       redirect_to(roo_url) unless current_user?(@user)
-     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,33 @@
 class User < ApplicationRecord
-  before_save { email.downcase! }
-  validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+  
+  #Followed Users
+  has_many :active_relationships, foreign_key: "follower_id",
+                                  class_name: "Relationship",
+                                  dependent: :destroy
+                                
+has_many :followed_users, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, foreign_key: "followed_id",
+                                    class_name: "Relationship",
+                                    dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
+  validates :name, presence: true, length: { maximum: 50 }
+  
+  
   has_secure_password 
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-
-
-end
+  
+  def following?(other_user)
+    active_relationships.find_by(followed_id: other_user.id)
+   end
+   def follow(other_user)
+    active_relationships.create!(followed_id: other_user.id)
+  end
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+   end
+  end
